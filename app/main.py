2d99d096a473876863a37295110f8573e765a3e6
Alexey Loktev <loktevalexey@gmail.com>
2026-08-18 17:21:45 +0100

    review fixes;

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from fastapi-hw-purpleschool!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def root():
+    return {"Score": 10}
