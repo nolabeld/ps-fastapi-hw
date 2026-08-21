05b6bd641fc6ba9e4f655bb2773ecc22cc94254d
Alexey Loktev <nolabeld@gmail.com>
2026-08-21 10:53:00 +0100

    add schemas and route validation for posts

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -1,23 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from .schemas import PostCreationRequest, PostPath, PostUpdateRequest
 
 posts_router = APIRouter(prefix="/posts")
 
+post_id_path = Depends(PostPath)
+
 
 @posts_router.get("/{id}")
-def get_post(id: int):
-    return {"id": id}
+def get_post(post_path: PostPath = post_id_path):
+    return {"id": post_path.id}
 
 
 @posts_router.post("/")
-def create_post():
-    return ""
+def create_post(data: PostCreationRequest):
+    return {"id": data.id, "message": f"created post: {data.content[:120]}"}
 
 
 @posts_router.put("/{id}")
-def update_post(id: int):
-    return id
+def update_post(
+    data: PostUpdateRequest,
+    post_path: PostPath = post_id_path,
+):
+    return {"id": post_path.id, "message": f"updated post: {data.content[:120]}"}
 
 
 @posts_router.delete("/{id}")
-def remove_post(id: int):
-    return "deleted"
+def remove_post(post_path: PostPath = post_id_path):
+    return {"id": post_path.id, "message": "deleted"}
