5e78331ce1c3cb6698e58ef87ad6aae82733691b
Alexey Loktev <nolabeld@gmail.com>
2026-08-22 10:50:12 +0100

    add openapi docs

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -1,30 +1,40 @@
 from fastapi import APIRouter, Depends
 
-from .schemas import PostCreationRequest, PostPath, PostUpdateRequest
+from .schemas import (
+    PostBase,
+    PostCreateRequest,
+    PostCreateResponse,
+    PostDeleteResponse,
+    PostPath,
+    PostUpdateRequest,
+    PostUpdateResponse,
+)
 
-posts_router = APIRouter(prefix="/posts")
+posts_router = APIRouter(prefix="/posts", tags=["Posts"])
 
 post_id_path = Depends(PostPath)
 
 
-@posts_router.get("/{id}")
+@posts_router.get("/{id}", response_model=PostBase)
 def get_post(post_path: PostPath = post_id_path):
-    return {"id": post_path.id}
+    return PostBase(id=post_path.id)
 
 
-@posts_router.post("/")
-def create_post(data: PostCreationRequest):
-    return {"id": data.id, "message": f"created post: {data.content[:120]}"}
+@posts_router.post("", response_model=PostCreateResponse)
+def create_post(data: PostCreateRequest):
+    return PostCreateResponse(id=data.id, message=f"created post: {data.content[:120]}")
 
 
-@posts_router.put("/{id}")
+@posts_router.put("/{id}", response_model=PostUpdateResponse)
 def update_post(
     data: PostUpdateRequest,
     post_path: PostPath = post_id_path,
 ):
-    return {"id": post_path.id, "message": f"updated post: {data.content[:120]}"}
+    return PostUpdateResponse(
+        id=post_path.id, message=f"updated post: {data.content[:120]}"
+    )
 
 
-@posts_router.delete("/{id}")
+@posts_router.delete("/{id}", response_model=PostDeleteResponse)
 def remove_post(post_path: PostPath = post_id_path):
-    return {"id": post_path.id, "message": "deleted"}
+    return PostDeleteResponse(id=post_path.id, message="deleted")
