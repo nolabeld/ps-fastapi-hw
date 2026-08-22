3c82ffd9ba890c18605468a2477dd78a96dec7d0
Alexey Loktev <nolabeld@gmail.com>
2026-08-22 10:55:06 +0100

    add descriptions and summaries on routes

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -15,17 +15,31 @@ posts_router = APIRouter(prefix="/posts", tags=["Posts"])
 post_id_path = Depends(PostPath)
 
 
-@posts_router.get("/{id}", response_model=PostBase)
+@posts_router.get(
+    "/{id}",
+    response_model=PostBase,
+    summary="Get post by ID",
+)
 def get_post(post_path: PostPath = post_id_path):
     return PostBase(id=post_path.id)
 
 
-@posts_router.post("", response_model=PostCreateResponse)
+@posts_router.post(
+    "",
+    response_model=PostCreateResponse,
+    summary="Create new post",
+    status_code=201,
+    description="""creates a new post, returns truncated content in message key""",
+)
 def create_post(data: PostCreateRequest):
     return PostCreateResponse(id=data.id, message=f"created post: {data.content[:120]}")
 
 
-@posts_router.put("/{id}", response_model=PostUpdateResponse)
+@posts_router.put(
+    "/{id}",
+    response_model=PostUpdateResponse,
+    summary="Update full post body by id",
+)
 def update_post(
     data: PostUpdateRequest,
     post_path: PostPath = post_id_path,
@@ -35,6 +49,10 @@ def update_post(
     )
 
 
-@posts_router.delete("/{id}", response_model=PostDeleteResponse)
+@posts_router.delete(
+    "/{id}",
+    response_model=PostDeleteResponse,
+    status_code=204,
+)
 def remove_post(post_path: PostPath = post_id_path):
     return PostDeleteResponse(id=post_path.id, message="deleted")
